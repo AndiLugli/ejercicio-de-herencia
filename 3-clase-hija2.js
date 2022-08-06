f91b3757e0f7c3e19534577bce813e8e918273c0
Andrés Lugli <lugli1995@gmail.com>
2022-08-06 13:04:26 -0300

    ejercicio realizado

---
+++ 3-clase-hija2.js
@@ -0,0 +1,33 @@
+class Cliente extends Persona {
+
+    static contadorClientes = 0;
+
+    constructor(nombre, apellido, edad , fechaRegistro){
+
+        super(nombre,apellido,edad);
+
+        this._fechaRegistro = fechaRegistro;
+
+        this._idCliente = ++Cliente.contadorClientes;
+    }
+
+    get idCliente (){
+
+        return this._idCliente;
+    }
+
+    get fechaRegistro (){
+
+        return this._fechaRegistro;
+    }
+
+    set fechaRegistro(fechaRegistro){
+
+        this._fechaRegistro = fechaRegistro;
+    }
+
+    toString (){
+
+        return super.toString + "n° de cliente: " + this._idCliente + ",Fecha de registro: " + this._fechaRegistro;
+    }
+}
